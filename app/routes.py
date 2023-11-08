5e5bdf37a00dcdac10a309505eeaf56d79386397
Italo Viana <italo_1002@live.com>
2023-11-07 21:59:23 -0300

    add return available berths route

--- app/routes.py
+++ app/routes.py
@@ -11,3 +11,14 @@ def index():
 @main.route("/berth/<id>", methods=['GET'])
 def berth(id):
   return render_template("berth.html", id = id)
+
+@main.route("/berths", methods=['GET'])
+def berths():
+  berths_values = []
+  for berth in BERTHS:
+    berths_values.append(str(berth))
+
+  berths_json = {
+    "berths": berths_values
+  }
+  return berths_json
