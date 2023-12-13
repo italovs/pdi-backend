44d239c461c90de038a949372fd3f4aa85b02119
JustinoCarneiro <justinocarneiro161@gmail.com>
2023-12-13 02:13:09 -0300

    alteracoes finais

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,18 @@ from flask import Flask
 from flask_socketio import SocketIO
 from ast import literal_eval
 from os import environ
+from flask_cors import CORS
+
 
 socket = SocketIO(cors_allowed_origins="*")
 BERTHS = literal_eval(environ.get('BERTHS'))
 
 def create_app():
   app = Flask(__name__)
+  CORS(app)
 
   from app.routes import main
   app.register_blueprint(main)
-  
-  from app.socket_routes import socket_bp
-  app.register_blueprint(socket_bp)
   
   socket.init_app(app)
   
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, request, current_app, jsonify
 from flask.templating import render_template
 from app import BERTHS
+from app import socket
+from queue import Queue, Empty
+from app.services.room_manager import RoomManager
+import time
+import threading
+import json
 
 main = Blueprint('main', __name__)
+
+speed = 0
+position_distance = 0
 
 @main.route("/")
 def index():
@@ -11,6 +20,16 @@ def index():
 @main.route("/berth/<id>", methods=['GET'])
 def berth(id):
   return render_template("berth.html", id = id)
+
+@main.route("/dados", methods=['POST'])
+def dados():
+  global speed, position_distance
+  body = json.loads(request.data.decode())
+  print(body)
+  speed = round(body["speed"], 2)
+  position_distance = round(body["present_position"], 2)
+
+  return jsonify({'status': 'success'})
 
 @main.route("/berths", methods=['GET'])
 def berths():
@@ -22,3 +41,72 @@ def berths():
     "berths": berths_values
   }
   return berths_json
+
+room_manager_main = RoomManager(BERTHS)
+queue = Queue()
+queue.put(room_manager_main)
+
+@socket.on('join')
+def join(room):
+  try:
+    room_manager = queue.get(block=False)
+  except Empty:
+    pass
+
+  if room_manager.room_state(room):
+    room_manager.join_room(room, request.sid)
+    queue.put(room_manager)
+  else:
+    room_manager.join_room(room, request.sid)
+    queue.put(room_manager)
+    thread = threading.Thread(target=send_data, args=(room, queue, current_app.app_context()))
+    thread.start()
+
+@socket.on('leave')
+def leave(room):
+  try:
+    room_manager = queue.get(block=False)
+  except Empty:
+    pass
+
+  room_manager.exit_room(room)
+  queue.put(room_manager)
+
+def send_data(room, queue, app_context):
+  global speed, position_distance
+  app_context.push()
+  room_manager = queue.get()
+  queue.put(room_manager)
+  state = True
+
+  while(state):
+    json = build_json(speed, speed, 0, position_distance, position_distance)
+    socket.emit("message", json, to=room)
+    
+    time.sleep(1/30)
+
+    try:
+      room_manager = queue.get(block=False)
+    except Empty:
+      pass
+
+    state = room_manager.room_state(room)
+    queue.put(room_manager)
+  
+def build_json(speed_a, speed_b, angle, distance_a, distance_b):
+  return {
+    "velocity": {
+      "a": speed_a,
+      "b": speed_b,
+      "unit": "cm/s"
+    },
+    "distance": {
+      "a": distance_a,
+      "b": distance_b,
+      "unit": "m"      
+    },
+    "angle": {
+      "value": angle,
+      "unit": "°"
+    }
+  }
